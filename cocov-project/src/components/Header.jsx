551fd2257d10dfe3d84e12dd351efb1d371cde7f
Godfred <essiengodfred88@gmail.com>
2024-10-13 20:02:22 +0000

    styled the header jsx with hambuger

--- cocov-project/src/components/Header.jsx
+++ cocov-project/src/components/Header.jsx
@@ -1,30 +1,40 @@
 import { Link, NavLink } from "react-router-dom"
-import Hamburger from "../Assets/Hamburger"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { HiBars3 } from "react-icons/hi2";
+import { useState } from "react";
+
 
 function Header() {
-  return (
-      <header className="w-screen absolute flex justify-center items-center border border-green-500 ">
-        <nav className="bg-[#76421A] py-6 px-6 h-10 w-full">
-            <div className="flex justify-end items-center sm:flex sm:items-center sm:justify-between">
-                <FontAwesomeIcon icon={faBars} className="text-white h-7 w-6 right"/>
-                <div className="w-[90%] flex justify-center">
-                    <Link to='/'>
-                        <p className="text-3xl text-white">CuCov</p>
-                    </Link>
-                    <div className="hidden sm:block w-[362px] justify-between">
-                        <NavLink to='/' className={({isActive}) => `${isActive ? 'text-blue-700' : 'text-red-600' }`}>Home</NavLink>
-                        <NavLink to='rate-alert' className={({isActive}) => `${isActive ? 'text-blue-700' : 'text-red-600' }`}>Rate alert</NavLink>
-                        <NavLink to='about' className={({isActive}) => `${isActive ? 'text-blue-700' : 'text-red-600' }`}>About Me</NavLink>
-                        <NavLink to='contact' className={({isActive}) => `${isActive ? 'text-blue-700' : 'text-red-600' }`}>Contact Me</NavLink>
-                    </div>
 
-                </div>
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [clickMenuClose, setClickMenuClose] = useState(false);
+
+    function handleMenuClose (){
+        let menu = document.querySelector('.menu-close');
+        menu.style.display = 'none';
+    }
+
+  return (
+    <header className="w-full">
+        <nav className="bg-white border py-6 px-8 md:px-32 drop-shadow-md w-full flex justify-between items-center">
+            <Link to='/' className="h-9 w-26">
+                <p className="text-2xl hover:scale-105 transition-all text-blue-600">CuCov</p>
+            </Link>
+            <div className="hidden sm:flex items-center gap-6">
+                <NavLink to='/' className={({isActive}) => `${isActive ? 'text-blue-600' : 'text-gray-900' } hover:border-b-2 hover:border-gray-300`}>Home</NavLink>
+                <NavLink to='rate-alert' className={({isActive}) => `${isActive ? 'text-blue-700' : 'text-gray-900'} hover:border-b-2 hover:border-gray-300`}>Rate -alert</NavLink>
+                <NavLink to='about' className={({isActive}) => `${isActive ? 'text-blue-700' : 'text-gray-900' } hover:border-b-2 hover:border-gray-300`}>About Me</NavLink>
+                <NavLink to='contact' className={({isActive}) => `${isActive ? 'text-blue-700' : 'text-gray-900' } hover:border-b-2 hover:border-gray-300`}>Contact Me</NavLink>
+            </div>
+            <div className="sm:hidden">
+                <HiBars3 className="text-xl text-blue-600 cursor-pointer" onClick={() => setIsMenuOpen(!isMenuOpen)}/>           
+            </div>
+            <div className={`menu-close absolute sm:hidden py-5 top-[86px] left-0 w-full bg-white flex flex-col items-center gap-6 font-semibold text-lg transform transition-transform ${clickMenuClose ? 'opacity-0' : 'opacity-100'} ${isMenuOpen ? 'opacity-100' : 'opacity-0'}`} style={{transition:'transform 0.3s ease', opacity:'0.3s ease'}}>
+                <NavLink to='rate-alert' onClick={() => setClickMenuClose(!clickMenuClose)} className={({isActive}) => `${isActive ? 'text-blue-700' : 'text-gray-900'} hover:border-b-2 hover:border-gray-300`}>Rate-alert</NavLink>
+                <NavLink to='about' onClick={handleMenuClose} className={({isActive}) => `${isActive ? 'text-blue-700' : 'text-gray-900' } hover:border-b-2 hover:border-gray-300`}>About Me</NavLink>
+                <NavLink to='contact' onClick={handleMenuClose} className={({isActive}) => `${isActive ? 'text-blue-700' : 'text-gray-900' } hover:border-b-2 hover:border-gray-300`}>Contact Me</NavLink>
             </div>
         </nav>
-        <Hamburger className="absolute z-10"/>
-      </header>
+    </header>
   )
 }
 
